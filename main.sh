e0070c8c77bd000c058318b60d479e32f8a7dab4
AlexyPulivelil <alexypulivelil@gmail.com>
2021-06-09 17:40:37 +0530

    Initial Commit

--- main.sh
+++ main.sh
@@ -1,5 +1,5 @@
-aws s3api create-bucket --bucket techino1bucket --region us-east-1
+aws s3api create-bucket --bucket "Add Bucket Name" --region us-east-1
 aws ec2 authorize-security-group-ingress --group-id sg-c0c302a4 --protocol tcp --port 22 --cidr 0.0.0.0/0
 aws ec2 authorize-security-group-ingress --group-id sg-c0c302a4 --protocol tcp --port 80 --cidr 0.0.0.0/0
 aws ec2 authorize-security-group-ingress --group-id sg-c0c302a4 --protocol tcp --port 443 --cidr 0.0.0.0/0
-aws ec2 run-instances --image-id ami-04bde106886a53080 --instance-type t2.micro --count 1 --region ap-south-1 --subnet-id subnet-877b79ef --key-name bot --security-group-ids sg-c0c302a4 --tag-specifications 'ResourceType=instance,Tags=[{Key=Company,Value=saaspect}]'
+aws ec2 run-instances --image-id ami-04bde106886a53080 --instance-type t2.micro --count 1 --region ap-south-1 --subnet-id "Add Subnet" --key-name "Add Key" --security-group-ids "Add sq name" --tag-specifications 'ResourceType=instance,Tags=[{Key=Alexy,Value=Techino}]'
